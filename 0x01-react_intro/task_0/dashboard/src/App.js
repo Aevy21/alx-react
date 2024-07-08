988b7eedb89c4de862ba73287e40382f5c53b202
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-07-08 13:44:49 +0200

    Update App.js

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -5,14 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="holberton_logo.png" alt="Holberton Logo" /> {/* Replace with your Holberton logo */}
+        <img src="holberton-logo.png" alt="Holberton Logo" /> 
         <h1>School dashboard</h1>
       </header>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
       <footer className="App-footer">
-        <p>Copyright 2024 - Holberton School</p> {/* Update with the current year */}
+        <p>Copyright 2024 - Holberton School</p> 
       </footer>
     </div>
   );
