9196b967b4105db5a83f2db9e794bb5531726faf
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-07-17 15:37:23 +0200

    Update 4-mutations.js

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -1,6 +1,6 @@
 import { Map } from 'immutable';
 
-// Creating the initial Immutable Map
+// Create the initial Immutable Map
 const map = Map({
     1: 'Liam',
     2: 'Noah',
@@ -10,11 +10,11 @@ const map = Map({
     6: 'Lucas',
 });
 
-// Creating ap2 by modifying values from map
+// Create map2 by modifying values from map
 const map2 = map.withMutations(mutableMap => {
     mutableMap.set(2, 'Benjamin');
     mutableMap.set(4, 'Olivia');
 });
 
-export default map;
-export default map2;
+// Export map and map2
+export default{ map, map2 };
