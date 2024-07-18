51e935efc7e46960847c8e7d7df35b868bafeaa6
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-07-18 14:51:03 +0200

    Update 4-mutations.js

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -1,7 +1,7 @@
 import { Map } from 'immutable';
 
-// Create and export a constant named map
-export const map = Map({
+// Create an Immutable Map named map
+const map = Map({
   1: 'Liam',
   2: 'Noah',
   3: 'Elijah',
@@ -10,6 +10,10 @@ export const map = Map({
   6: 'Lucas'
 });
 
-// Export a second constant named map2
-// Modify the value for index 2 to 'Benjamin' and index 4 to 'Oliver'
-export const map2 = map.set(2, 'Benjamin').set(4, 'Oliver');
+// Modify the values for index 2 and index 4 in map to 'Benjamin' and 'Oliver', respectively
+const map2 = map.set(2, 'Benjamin').set(4, 'Oliver');
+
+export default {
+  map,
+  map2
+};
