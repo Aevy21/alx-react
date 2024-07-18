9866a87ec2f6d8457906e4e4f661a023f8008c7a
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-07-18 23:02:11 +0200

    Update 6-deeply.js

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,16 +1,5 @@
-import { Map, List } from 'immutable';
+import { Map } from 'immutable';
 
-// Function to merge deeply and return as List
-export default function mergeDeeplyElements(page1, page2) {
-  // Convert the plain objects to Immutable Maps
-  const map1 = Map(page1);
-  const map2 = Map(page2);
+const mergeDeeplyElements = (page1, page2) => Map(page1).mergeDeep(Map(page2));
 
-  // Deeply merge the two Maps
-  const mergedMap = map1.mergeDeep(map2);
-
-  // Convert the merged Map to a List
-  const resultList = List(mergedMap.values());
-
-  return resultList;
-}
+export default mergeDeeplyElements;
