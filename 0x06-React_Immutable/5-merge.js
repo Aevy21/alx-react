27d1f9f9dc6c74caec9a4307b3d79e4131aa33f4
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-07-18 22:59:02 +0200

    Update 5-merge.js

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,16 +1,5 @@
 import { List, Map } from 'immutable';
 
-// Function to concatenate two arrays and return a List
-function concatElements(page1, page2) {
-  return List(page1).concat(List(page2));
-}
+export const concatElements = (page1, page2) => List(page1).concat(List(page2));
 
-// Function to merge two objects and return a List
-function mergeElements(page1, page2) {
-  const map1 = Map(page1);
-  const map2 = Map(page2);
-  const mergedMap = map1.merge(map2);
-  return List(mergedMap.values());
-}
-
-export { concatElements, mergeElements };
+export const mergeElements = (page1, page2) => Map(page1).merge(Map(page2));
